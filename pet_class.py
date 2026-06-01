e6c543991d306a56f1307a185e7332c475c3c6f7
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-06-01 21:55:04 +0800

    added constructor with name, animal type and age

--- pet_class.py
+++ pet_class.py
@@ -1,1 +1,7 @@
 class Pet:
+
+    # CONSTRUCTOR
+    def __init__(self, name="", animal_type="", age=0):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
