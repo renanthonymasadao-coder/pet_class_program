52dae66bd43481b58f2bf1c14f51403b9a6ac6c9
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-06-01 22:37:10 +0800

    adding program head and user input to libraries import

--- pet_class_main.py
+++ pet_class_main.py
@@ -1,1 +1,12 @@
 from pet_class import Pet
+
+# PROGRAM HEADER
+print("===================================")
+print("      PET CLASS PROGRAM")
+print("   OOP + ENCAPSULATION DEMO")
+print("===================================")
+
+# USER INPUT
+pet_name = input("Enter Pet Name: ")
+animal_type = input("Enter Animal Type: ")
+pet_age = int(input("Enter Pet Age: "))
