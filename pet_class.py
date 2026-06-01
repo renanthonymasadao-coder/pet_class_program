9ada28c7fa6135be33f3f786c2d93ffb885fbf57
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-06-01 22:05:22 +0800

    adding setter methods to set the standard in the program

--- pet_class.py
+++ pet_class.py
@@ -5,6 +5,16 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+
+   # SET METHODS
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
         
     # GET METHODS
     def get_name(self):
