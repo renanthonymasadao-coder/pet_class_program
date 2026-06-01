76c066cf71f90e2044a48b9a2fb5fe5a8c1d1d8a
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-06-01 22:47:15 +0800

    adding create object with set values using set methods and displayed info, and last finalized pet class program code

--- pet_class_main.py
+++ pet_class_main.py
@@ -10,3 +10,16 @@ print("===================================")
 pet_name = input("Enter Pet Name: ")
 animal_type = input("Enter Animal Type: ")
 pet_age = int(input("Enter Pet Age: "))
+
+# CREATE OBJECT
+my_pet = Pet()
+
+# SET VALUES USING SET METHODS
+my_pet.set_name(pet_name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(pet_age)
+
+# DISPLAY PET INFO
+my_pet.display_pet_info()
+
+print("\nThank you for using the program!")
